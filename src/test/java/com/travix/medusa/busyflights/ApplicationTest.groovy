1ebd7caf0c76b5991c97ee17d81bb16367bb8d01
Cristian Morales Alvarez <cristianmorales24131@gmail.com>
2022-03-25 15:02:03 +0000

    Testing ready to be used!

---
+++ src/test/java/com/travix/medusa/busyflights/ApplicationTest.groovy
@@ -0,0 +1,12 @@
+package com.travix.medusa.busyflights
+
+import spock.lang.Specification
+
+class ApplicationTest extends Specification {
+
+    def "Failing test" () {
+        expect:
+        false
+    }
+
+}
